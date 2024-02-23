939772cb82ef22a03c12983f36c6a77034392395
大熊 <androidzk@163.com>
2024-02-23 15:38:04 +0800

    替换logo，新增开机自启动

--- app/src/main/java/com/visz/tv/App.java
+++ app/src/main/java/com/visz/tv/App.java
@@ -3,16 +3,15 @@ package com.visz.tv;
 import androidx.multidex.MultiDexApplication;
 
 
-public class App  extends MultiDexApplication {
+public class App extends MultiDexApplication {
     private static App inst;
-    public static App inst()
-    {
+
+    public static App inst() {
         return inst;
     }
 
     @Override
-    public void onCreate()
-    {
+    public void onCreate() {
         super.onCreate();
         inst = this;
     }
